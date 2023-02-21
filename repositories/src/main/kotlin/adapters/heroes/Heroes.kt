7d3810eed23dd36c5effcbfadbfcb30e70b09c3a
crichardd <crichard11@myges.fr>
2023-02-21 12:17:16 +0100

    ajout de toute les columns à l'interface

--- repositories/src/main/kotlin/adapters/heroes/Heroes.kt
+++ repositories/src/main/kotlin/adapters/heroes/Heroes.kt
@@ -1,11 +1,15 @@
 package adapters.heroes
 
-import org.ktorm.schema.Table
-import org.ktorm.schema.uuid
-import org.ktorm.schema.varchar
+import org.ktorm.schema.*
 
 internal object Heroes: Table<KtormHeroes>("HEROES") {
     val id = uuid("id").primaryKey().bindTo { it.id }
     val name = varchar("name").bindTo { it.name }
-    //toutes la dataclass
+    val healthPoints = int("healthPoints").bindTo { it.healthPoints }
+    val experiencePoints = int("experiencePoints").bindTo { it.experiencePoints }
+    val power = int("power").bindTo { it.power }
+    val armor = int("armor").bindTo { it.armor }
+    val specialty = varchar("specialty").bindTo { it.specialty }
+    val rarity = varchar("rarity").bindTo { it.rarity }
+    val level = int("level").bindTo { it.level }
 }
